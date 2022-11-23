66145adfdfb4b7dd4eee2128e0d9e52bad0c00b7
Joslin Some <joslin-some@uiowa.edu>
2022-11-23 15:43:17 -0600

    starter

--- team-portfolio/src/Components/option.js
+++ team-portfolio/src/Components/option.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import "./options.css"
+import "../App.css"
+import {Link} from "react-router-dom";
+
 function Option(props) {
     const {title} = props
     return (
-        <div className='option'>
-            <h2 className="h2">{title}</h2>
+
+        <div  >
+
+            <Link to='/joslin' className='option'>
+                <h2 className="h2">{title}</h2>
+            </Link>
         </div>
     );
 }
