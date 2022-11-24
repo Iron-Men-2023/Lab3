2f976448608b94b1a3dd0a357e228fd1ded4207c
Adnane Ezouhri <adnane-ezouhri@uiowa.edu>
2022-11-24 15:43:40 -0600

    Added a slide show on my page. Imgaes not showing tho

--- team-portfolio/src/adnane.js
+++ team-portfolio/src/adnane.js
@@ -1,45 +1,67 @@
 import React from 'react';
 import profile from "./images/Adnane-profile-picture.jpg"
 import './adnane.css'
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+const slideImages = [
+	{
+		url: './image/French-flag.png',
+		caption: 'Slide 1'
+	},
+	{
+		url: './image/Moroccan-flag.png',
+		caption: 'Slide 2'
+	},
+	{
+		url: {profile},
+		caption: 'Slide 3'
+	},
+];
+
 const Adnane = () => {
-  return (
-<body> 
-<article class="article">
-		<div class="intro-container">
-				<div class="circle" >
-					<img src={profile}/>
+	return (
+		<body>
+			<article class="article">
+				<div class="intro-container">
+					<div class="circle" >
+						<img src={profile} />
+					</div>
+
+					<div class="intro-text-area">
+						<header>
+							<h1>Hello there, it's <b>Adnane Ezouhri</b>.</h1>
+						</header>
+						<p>I'm an <strong>Electrical and Computer Engineer</strong> in the Iron-Men team!</p>
+					</div>
 				</div>
+			</article>
 
-				<div class="intro-text-area">
-					<header>
-						<h1>Hello there, it's <b>Adnane Ezouhri</b>.</h1>
-					</header>
-					<p>I'm an <strong>Electrical and Computer Engineer</strong> in the Iron-Men team!</p>
+			<article class="article">
+				<div class="intro-container">
+					<div class="intro-text-area">
+						<header>
+							<h2> A little to know about me...</h2>
+						</header>
+
+					</div>
 				</div>
-			</div>
-</article>
+			</article>
 
-<article class="article">
-		<div class="intro-container">
-			<div class="intro-text-area">
-				<header>
-					<h2> A little to know about me...</h2>
-				</header>
-				<div className="slideshow">
-      				<div className="slideshowSlider">
-        				<div className="slide">
-
+			<div className="slide-container">
+							<Slide>
+								{slideImages.map((slideImage, index) => (
+									<div className="each-slide" key={index}>
+										<div style={{ 'backgroundImage': `url(${slideImage.url})` }}>
+											<span>{slideImage.caption}</span>
+										</div>
+									</div>
+								))}
+							</Slide>
 						</div>
-      				</div>
-    			</div>
-			</div>
-		</div>
-</article>
-
-</body>
+		</body>
 
 
-  );
+	);
 };
-  
+
 export default Adnane;
