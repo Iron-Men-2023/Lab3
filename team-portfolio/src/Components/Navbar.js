6415a0a8048a21524dda6b26c1db8c98043872b1
Ben DeSollar <ben.desollar01@gmail.com>
2022-11-30 17:26:27 -0600

    Initial commit of page and all of my images.

--- team-portfolio/src/Components/Navbar.js
+++ team-portfolio/src/Components/Navbar.js
@@ -30,9 +30,12 @@ function Navbar(props) {
 
                     </li>
                     <li>
-                        <ExternalLink href="https://bdesollar.github.io/" className='nav-links'>
+                        {/*<ExternalLink href="https://bdesollar.github.io/" className='nav-links'>*/}
+                        {/*    <h2>Ben</h2>*/}
+                        {/*</ExternalLink>*/}
+                        <Link to='/ben' className='nav-links'>
                             <h2>Ben</h2>
-                        </ExternalLink>
+                        </Link>
                     </li>
                     <li>
                         <Link to='/Justin' className='nav-links'>
